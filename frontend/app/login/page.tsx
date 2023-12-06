acc63474aa3ec9ca2b4748114d6dff863776bc0c
Toshiki Takakubo <219tosiki@gmail.com>
2023-12-06 21:43:52 +0900

    Welcomeコンポーネント作成

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import LoginForm from "@/app/components/Login/LoginForm";
 import SignupLink from "@/app/components/Login/SignupLink";
+import Welcom from "../components/Login/Welcome";
 
 const page = () => {
   return (
     <div>
+      <Welcom />
       <LoginForm />
       <SignupLink />
     </div>

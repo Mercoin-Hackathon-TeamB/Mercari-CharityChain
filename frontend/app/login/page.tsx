d24e423eaf4c1db9a6db47fc9a1fdfcc1c3ea003
Toshiki Takakubo <219tosiki@gmail.com>
2023-12-06 21:35:01 +0900

    SignupLinkコンポーネント作成

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import LoginForm from "@/app/components/Login/LoginForm";
+import SignupLink from "@/app/components/Login/SignupLink";
 
 const page = () => {
   return (
     <div>
       <LoginForm />
+      <SignupLink />
     </div>
   );
 };

a3b094874a2b77bbbafbd81789370513d4423586
Toshiki Takakubo <219tosiki@gmail.com>
2023-12-08 00:30:58 +0900

    商品一覧ページのUI改善

--- frontend/app/components/Product/ProductList.tsx
+++ frontend/app/components/Product/ProductList.tsx
@@ -1,34 +1,67 @@
-import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+"use client";
+import React, { useState } from "react";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  useDisclosure,
+} from "@nextui-org/react";
+import ProductModal from "./ProductModal";
 import list from "./Products.json";
-import Link from "next/link";
 
-const Products = () => {
+interface Product {
+  id: number;
+  title: string;
+  img: string;
+  price: string;
+}
+
+const ProductList = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+
+  const handleCardClick = (product: Product) => {
+    setSelectedProduct(product);
+    onOpen();
+  };
+
   return (
-    <div className="m-5">
-      <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-        {list.map((item, index) => (
-          <Link href={`/purchase/${item.id}`}>
-            <Card shadow="sm" key={index} isPressable className="max-w-xs mt-2">
-              <CardBody className="overflow-visible p-0">
-                <Image
-                  shadow="sm"
-                  radius="lg"
-                  width="100%"
-                  alt={item.title}
-                  className="object-cover"
-                  src={item.img}
-                />
-              </CardBody>
-              <CardFooter className="text-small justify-between">
-                <b>{item.title}</b>
-                <p className="text-default-500">{item.price}</p>
-              </CardFooter>
-            </Card>
-          </Link>
+    <div className="m-4">
+      <div className="gap-6 grid grid-cols-2 sm:grid-cols-4 mt-4">
+        {list.map((item) => (
+          <Card
+            shadow="sm"
+            key={item.id}
+            isPressable
+            onPress={() => handleCardClick(item)}
+          >
+            <CardBody className="overflow-visible p-0">
+              <Image
+                shadow="sm"
+                radius="lg"
+                width="100%"
+                alt={item.title}
+                className="w-full object-cover"
+                src={item.img}
+              />
+            </CardBody>
+            <CardFooter className="text-large justify-between">
+              <b>{item.title}</b>
+              <p className="text-default-500">{item.price}</p>
+            </CardFooter>
+          </Card>
         ))}
       </div>
+      {selectedProduct && (
+        <ProductModal
+          isOpen={isOpen}
+          onOpenChange={onClose}
+          product={selectedProduct}
+        />
+      )}
     </div>
   );
 };
 
-export default Products;
+export default ProductList;

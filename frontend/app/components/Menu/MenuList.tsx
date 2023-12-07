7ff184aac58f72c0caa0b0923cf16feebccfe685
Toshiki Takakubo <219tosiki@gmail.com>
2023-12-07 23:25:12 +0900

    MenuページのUI改善

--- frontend/app/components/Menu/MenuList.tsx
+++ frontend/app/components/Menu/MenuList.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { Button, Card } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Image,
+  Link,
+} from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 
 const MenuPage = () => {
@@ -27,7 +34,7 @@ const MenuPage = () => {
 
         if (response.ok) {
           const data = await response.json();
-          setBalance(data.balance); // 残高を設定
+          setBalance(data.balance);
         } else {
           console.error("Failed to fetch balance");
         }
@@ -39,27 +46,50 @@ const MenuPage = () => {
     fetchBalance();
   }, []);
 
-  const handlePurchase = () => {
-    // `/products` ページに遷移
-    router.push("/products");
-  };
-
-  const handleReceiveRequest = () => {
-    router.push("/select");
-  };
-
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="text-lg font-bold">Your Balance: {balance} BTC</div>
-      <div className="mt-4">
-        <Button onClick={handlePurchase} color="primary">
-          メルカリサービスを利用する
-        </Button>
+    <div>
+      <div className="text-lg font-bold justify-center items-center flex">
+        Your Balance: {balance} BTC
       </div>
-      <div className="mt-4">
-        <Button onClick={handleReceiveRequest} color="primary">
-          受取り申請
-        </Button>
+      <div className="flex justify-center items-center">
+        <div className="flex flex-row justify-center items-center space-x-4">
+          <div className="mt-4">
+            <Link href="/products">
+              <Card shadow="sm" isPressable className="max-w-xs mt-2 mr-4">
+                <CardBody className="overflow-visible p-0">
+                  <Image
+                    shadow="sm"
+                    radius="lg"
+                    width="100%"
+                    className="object-cover"
+                    src="/shop.png"
+                  />
+                </CardBody>
+                <CardFooter className="text-small justify-between">
+                  <b>サービスを利用する</b>
+                </CardFooter>
+              </Card>
+            </Link>
+          </div>
+          <div className="mt-4">
+            <Link href="/select">
+              <Card shadow="sm" isPressable className="max-w-xs mt-2">
+                <CardBody className="overflow-visible p-0">
+                  <Image
+                    shadow="sm"
+                    radius="lg"
+                    width="100%"
+                    className="object-cover"
+                    src="/apply.png"
+                  />
+                </CardBody>
+                <CardFooter className="text-small justify-between">
+                  <b>寄付を受けとる申請</b>
+                </CardFooter>
+              </Card>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
